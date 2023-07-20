0986ed6a5f5933160fa3f841ab8872b4d267cc22
Wolfgang Fahl <wf@bitplan.com>
2023-07-20 11:57:08 +0200

    fixes #4

--- nicescad/webserver.py
+++ nicescad/webserver.py
@@ -11,17 +11,44 @@ from nicegui import ui, app
  
 class WebServer:
     """
-    webserver
+    A class used to manage a web server. 
+
+    ...
+
+    Attributes
+    ----------
+    oscad : OpenScad
+        a helper object for handling OpenScad operations
+    home : method
+        a method for generating the home page of the web server
+    settings : method
+        a method for generating the settings page of the web server
+
+    Methods
+    -------
+    render(click_args):
+        Renders the OpenScad string.
+    menu():
+        Creates a menu with a link to the project's GitHub page.
+    home():
+        Creates the home page with a 3D viewer and code editor.
+    settings():
+        Creates the settings page with a menu link.
+    run(host, port):
+        Runs the UI of the web server.
     """
 
     def __init__(self):
         """
-        constructor
+        Constructs the necessary attributes for the WebServer object.
+
+        Parameters
+        ----------
+        None
         """
-        self.oscad=OpenScad()
+        self.oscad = OpenScad()
         app.add_static_files('/stl', self.oscad.tmp_dir)
-        pass
-    
+
         @ui.page('/')
         def home():
             self.home()
@@ -31,37 +58,68 @@ class WebServer:
             self.settings()
  
     
-    def render(self,click_args):
+    def render(self, click_args):
         """
-        render the openscad str
+        Renders the OpenScad string and updates the 3D scene with the result.
+
+        Parameters
+        ----------
+        click_args : object
+            The click event arguments.
         """
-        openscad_str=self.code_area.value
-        stl=self.oscad.openscad_str_to_file(openscad_str)
-        self.scene.stl("/stl/tmp.stl").move(x=-0.5).scale(0.06)
-        pass
+        openscad_str = self.code_area.value
+        stl = self.oscad.openscad_str_to_file(openscad_str)
+        with self.scene:
+            self.scene.stl("/stl/tmp.stl").move(x=-0.5).scale(0.06)
     
     def menu(self):
+        """
+        Adds a link to the project's GitHub page in the web server's menu.
+
+        Parameters
+        ----------
+        None
+        """
         ui.link('nicescad on GitHub', 'https://github.com/WolfgangFahl/nicescad')
        
     def home(self):
+        """
+        Generates the home page with a 3D viewer and a code editor.
+
+        Parameters
+        ----------
+        None
+        """
         self.menu()
         with ui.splitter() as splitter:
             with splitter.before:
-                self.code_area = ui.textarea(value='cube(5);').props('clearable')
-                ui.button('Render', on_click=self.render)
-            with splitter.after:
                 with ui.scene(width=1024, height=768) as scene:
-                    self.scene=scene
+                    self.scene = scene
                     scene.spot_light(distance=100, intensity=0.1).move(-10, 0, 10)
-                    teapot = 'https://upload.wikimedia.org/wikipedia/commons/9/93/Utah_teapot_(solid).stl'
-                    scene.stl(teapot).scale(0.2).move(-3, 4)
+            with splitter.after:
+                self.code_area = ui.textarea(value='cube(10);').props('clearable')
+                ui.button('Render', on_click=self.render)
         
     def settings(self):
+        """
+        Generates the settings page with a link to the project's GitHub page.
+
+        Parameters
+        ----------
+        None
+        """
         self.menu()
        
   
-    def run(self, host, port):
+    def run(self, host: str, port: int):
         """
-        run the ui
+        Runs the UI of the web server.
+
+        Parameters
+        ----------
+        host : str
+            The host address of the web server.
+        port : int
+            The port number of the web server.
         """
         ui.run(title=Version.name, host=host, port=port, reload=False)
