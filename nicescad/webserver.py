67e9b91f8f0328024ea8e9ea1effbfc5a8e197c8
Wolfgang Fahl <wf@bitplan.com>
2023-09-14 08:52:21 +0200

    refactors

--- nicescad/webserver.py
+++ nicescad/webserver.py
@@ -3,7 +3,6 @@ Created on 2023-06-19
 
 @author: wf
 """
-from typing import Optional
 from nicescad.version import Version
 from nicescad.openscad import OpenScad
 from nicescad.axes_helper import AxesHelper
@@ -13,20 +12,25 @@ from nicegui import ui, app
 from pathlib import Path
 
 import os
-import sys
-import requests
-import traceback
 from nicegui.events import ColorPickEventArguments
+from ngwidgets.input_webserver import InputWebserver
+from ngwidgets.webserver import WebserverConfig
 
-class WebServer:
+class WebServer(InputWebserver):
     """WebServer class that manages the server and handles OpenScad operations.
 
     Attributes:
         oscad (OpenScad): An OpenScad object that aids in performing OpenScad operations.
     """
+    @classmethod
+    def get_config(cls)->WebserverConfig:
+        copy_right="(c)2023 Wolfgang Fahl"
+        config=WebserverConfig(copy_right=copy_right,version=Version(),default_port=9859)
+        return config
 
     def __init__(self):
         """Constructs all the necessary attributes for the WebServer object."""
+        InputWebserver.__init__(self,config=WebServer.get_config())
         self.oscad = OpenScad(scad_prepend="""//https://en.wikibooks.org/wiki/OpenSCAD_User_Manual/Other_Language_Features#$fa,_$fs_and_$fn
 // default number of facets for arc generation
 $fn=30;
@@ -43,9 +47,7 @@ example();"""
         self.stl_name="result.stl"
         self.stl_color="#57B6A9"
         self.stl_object=None
-        self.is_local=False
         app.add_static_files('/stl', self.oscad.tmp_dir)
-        self.log_view=None
         self.do_trace=True
         self.html_view=None
         self.axes_view=None
@@ -55,9 +57,8 @@ example();"""
             await self.home()
             
         @ui.page('/settings')
-        def settings():
-            self.settings()
-            
+        async def settings():
+            await self.settings()
             
     @classmethod
     def examples_path(cls)->str:
@@ -65,21 +66,6 @@ example();"""
         path = os.path.join(os.path.dirname(__file__), '../nicescad_examples')
         path = os.path.abspath(path)
         return path
- 
-    def handle_exception(self, e: BaseException, trace: Optional[bool] = False):
-        """Handles an exception by creating an error message.
-
-        Args:
-            e (BaseException): The exception to handle.
-            trace (bool, optional): Whether to include the traceback in the error message. Default is False.
-        """
-        if trace:
-            self.error_msg = str(e) + "\n" + traceback.format_exc()
-        else:
-            self.error_msg = str(e)
-        if self.log_view:
-            self.log_view.push(self.error_msg)
-        print(self.error_msg,file=sys.stderr)
 
     async def render(self, _click_args=None):
         """Renders the OpenScad string and updates the 3D scene with the result.
@@ -108,36 +94,7 @@ example();"""
                     self.stl_object.material(self.stl_color)
         except BaseException as ex:
             self.handle_exception(ex,self.do_trace)  
-        self.progress_view.visible=False  
-            
-    def do_read_input(self, input_str: str):
-        """Reads the given input.
-
-        Args:
-            input_str (str): The input string representing a URL or local path.
-        """
-        if input_str.startswith('http://') or input_str.startswith('https://'):
-            response = requests.get(input_str)
-            if response.status_code == 200:
-                return response.text
-            else:
-                raise Exception(f'Unable to retrieve data from URL: {input_str}')
-        else:
-            if os.path.exists(input_str):
-                with open(input_str, 'r') as file:
-                    return file.read()
-            else:
-                raise Exception(f'File does not exist: {input_str}')
-    
-    async def read_and_optionally_render(self,input_str):
-        """Reads the given input and optionally renders the given input
-
-        Args:
-            input_str (str): The input string representing a URL or local file.
-        """
-        self.read_input(input_str)
-        if self.render_on_load:
-            await self.render(None)
+        self.progress_view.visible=False
         
     def read_input(self, input_str: str):
         """Reads the given input and handles any exceptions.
@@ -174,67 +131,6 @@ example();"""
                 input_file=pick_list[0]
                 await self.read_and_optionally_render(input_file)
     pass
-
-    async def reload_file(self):
-        """
-        reload the input file
-        """
-        input_str=self.input
-        if os.path.exists(input_str):
-            input_str=os.path.abspath(input_str)
-        allowed_urls=[
-            "https://raw.githubusercontent.com/WolfgangFahl/nicescad/main/examples/",
-            "https://raw.githubusercontent.com/openscad/openscad/master/examples/",
-            self.examples_path(),
-            self.root_path
-        ]
-        if not self.is_local:
-            allowed=False
-            for allowed_url in allowed_urls:
-                if input_str.startswith(allowed_url):
-                    allowed=True
-        if not allowed:
-            ui.notify("only white listed URLs and Path inputs are allowed")
-        else:    
-            await self.read_and_optionally_render(self.input)
-            
-    
-    def link_button(self, name: str, target: str, icon_name: str):
-        """
-        Creates a button with a specified icon that opens a target URL upon being clicked.
-    
-        Args:
-            name (str): The name to be displayed on the button.
-            target (str): The target URL that should be opened when the button is clicked.
-            icon_name (str): The name of the icon to be displayed on the button.
-    
-        Returns:
-            The button object.
-        """
-        with ui.button(name,icon=icon_name) as button:
-            button.on("click",lambda: (ui.open(target)))
-        return button
-    
-    
-    def tool_button(self,tooltip:str,icon:str,handler:callable=None,toggle_icon:str=None)->ui.button:
-        """
-        Creates an  button with icon that triggers a specified function upon being clicked.
-    
-        Args:
-            tooltip (str): The tooltip to be displayed.
-            icon (str): The name of the icon to be displayed on the button.
-            handler (function): The function to be called when the button is clicked.
-            toggle_icon (str): The name of an alternative icon to be displayed when the button is clicked.
-    
-        Returns:
-            ui.button: The icon button object.
-            
-        valid icons may be found at:    
-            https://fonts.google.com/icons
-        """
-        icon_button=ui.button("",icon=icon, color='primary').tooltip(tooltip).on("click",handler=handler)  
-        icon_button.toggle_icon=toggle_icon
-        return icon_button   
     
     def setup_pygments(self):
         """
@@ -243,31 +139,6 @@ example();"""
         pygments_css_file=(Path(__file__).parent / 'web'/'static' / 'css'/ 'pygments.css')
         pygments_css= pygments_css_file.read_text()
         ui.add_head_html(f"<style>{pygments_css}</style>")
- 
-        
-    def setup_menu(self):
-        """Adds a link to the project's GitHub page in the web server's menu."""
-        with ui.header() as self.header:
-            self.link_button("home","/","home")
-            self.link_button("settings","/settings","settings")
-            self.link_button("github",Version.cm_url,"bug_report")
-            self.link_button("chat",Version.chat_url,"chat")
-            self.link_button("help",Version.doc_url,"help")
-    
-    def setup_footer(self):
-        """
-        setup the footer
-        """
-        with ui.footer() as self.footer:
-            ui.label("(c)2023 Wolfgang Fahl")
-            ui.link("Powered by nicegui","https://nicegui.io/").style("color: #fff") 
-  
-    def input_changed(self,cargs):
-        """
-        react on changed input
-        """
-        self.input=cargs.value
-        pass
     
     def code_changed(self,cargs):
         """
@@ -275,21 +146,6 @@ example();"""
         """
         self.code=cargs.value
         pass
-    
-    def toggle_icon(self,button:ui.button):
-        """
-        toggle the icon of the given button
-        
-        Args:
-            ui.button: the button that needs the icon to be toggled
-        """
-        if hasattr(button,"toggle_icon"):
-            # exchange icon with toggle icon
-            toggle_icon=button._props["icon"]
-            icon=button.toggle_icon
-            button._props["icon"]=icon
-            button.toggle_icon=toggle_icon
-        button.update()
     
     async def highlight_code(self,_cargs):
         """
@@ -404,24 +260,17 @@ example();"""
         if self.args.input:
             await self.read_and_optionally_render(self.args.input)
         
-    def settings(self):
+    def configure_settings(self):
         """Generates the settings page with a link to the project's GitHub page."""
-        self.setup_menu()
-        ui.checkbox('debug with trace', value=True).bind_value(self, "do_trace")
-        ui.checkbox('render on load',value=self.render_on_load).bind_value(self,"render_on_load")
         sp_input=ui.textarea("scad prepend",value=self.oscad.scad_prepend).props("cols=80")
         sp_input.bind_value(self.oscad,"scad_prepend")
-        self.setup_footer()
-       
-    def run(self, args):
-        """Runs the UI of the web server.
-
-        Args:
-            args (list): The command line arguments.
-        """
-        self.args=args
-        self.is_local=args.local
-        root_path=args.root_path if args.root_path else WebServer.examples_path()
+        
+    def configure_run(self):
+        root_path=self.args.root_path if self.args.root_path else WebServer.examples_path()
         self.root_path=os.path.abspath(root_path) 
-        self.render_on_load=args.render_on_load
-        ui.run(title=Version.name, host=args.host, port=args.port, show=args.client,reload=False)
+        self.allowed_urls=[
+            "https://raw.githubusercontent.com/WolfgangFahl/nicescad/main/examples/",
+            "https://raw.githubusercontent.com/openscad/openscad/master/examples/",
+            self.examples_path(),
+            self.root_path
+        ]
